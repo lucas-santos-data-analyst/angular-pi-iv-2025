1138536b11187c047ef4fb2899970591c217599c
Lucas Adriano <145597348+lucas-santos-data-analyst@users.noreply.github.com>
2025-08-28 23:42:04 +0000

    SegundaAula-ModLivros

--- aulas-angular/src/app/app-routing-module.ts
+++ aulas-angular/src/app/app-routing-module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'livros',
+    loadChildren: () => import('./livros/livros-module').then(m => m.LivrosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
